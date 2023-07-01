44b6c3f68dc093594381ebb7db01020db6ef73ce
griffio <griffio@users.noreply.github.com>
2023-07-01 13:46:59 +0100

    update kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    application
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
 }
 
 group = "org.example"
@@ -10,15 +11,19 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("griffio.client.ApplicationKt")
+}
+
 dependencies {
-    implementation("io.ktor:ktor-client-core-jvm:2.0.2")
-    implementation("io.ktor:ktor-client-java:2.0.2")
-    implementation("io.ktor:ktor-client-content-negotiation:2.0.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.0.2")
+    implementation("io.ktor:ktor-client-core-jvm:2.2.4")
+    implementation("io.ktor:ktor-client-java:2.2.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
     // a recent version of kotlinx-serialization-json will be pulled in
     // uncomment below to specify exact version
-    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    testImplementation(kotlin("test"))
+    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.4.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.22")
 }
 
 tasks.test {
@@ -27,6 +32,6 @@ tasks.test {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
